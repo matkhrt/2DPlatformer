28201a431829c5ca2ece9629b9a1c027c0b2deda
MatinKharati <matin.asr@gmail.com>
2025-09-12 16:31:38 +0300

    PlayerDevelopment

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,12 +3,93 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private Animator anim;
 
+    private float xInput;
+
+    private bool facingRight = true;
+    private int facingDir = 1;
+
+    [Header("Movement")]
     [SerializeField]
-    private float speed;
+    private float moveSpeed;
+    [SerializeField]
+    private float jumpForce;
+
+    [Header("Collision info")]
+    [SerializeField]
+    private float groundCheckDistance;
+    [SerializeField]
+    private LayerMask whatIsGround;
+    private bool isGrounded;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
     }
+
+    private void Update()
+    {
+        HandleCollision();
+        HandleInput();
+        HandleMovement();
+        HandleFlip();
+        HandleAnimations();
+    }
+
+    private void HandleInput()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y - groundCheckDistance));
+    }
+
+    private void HandleCollision()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+    }
+
+    private void HandleAnimations()
+    {
+        anim.SetFloat("xVelocity", rb.linearVelocity.x);
+        anim.SetFloat("yVelocity", rb.linearVelocity.y);
+        anim.SetBool("isGrounded", isGrounded);
+    }
+
+    private void HandleMovement()
+    {
+        rb.linearVelocity = new Vector2(xInput * moveSpeed * Time.deltaTime, rb.linearVelocity.y);
+    }
+
+    private void HandleFlip()
+    {
+        if (rb.linearVelocity.x < 0 && facingRight || rb.linearVelocity.x > 0 && !facingRight)
+        {
+            Flip();
+            
+        }
+    }
+    private void Flip()
+    {
+        facingDir = facingDir * -1;
+        transform.Rotate(0, 180, 0);
+        facingRight = !facingRight;
+    }
+
+   
 }
